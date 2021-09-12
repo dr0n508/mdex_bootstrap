332fee82ed360f2612add28554e6204ead23c002
dr0n <kornienko.andriy@gmail.com>
2021-09-12 11:53:38 +0300

    first content, ready to go

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -8,7 +8,7 @@
 //
 // Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
 
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
 
 
 // Option 2
@@ -18,5 +18,41 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
+
+
+$(document).ready(function() {
+
+  //countdown
+  function makeTimer() {
+
+    //		var endTime = new Date("29 April 2018 9:56:00 GMT+01:00");
+    var endTime = new Date("29 October 2021 9:56:00 GMT+01:00");
+    endTime = (Date.parse(endTime) / 1000);
+
+    var now = new Date();
+    now = (Date.parse(now) / 1000);
+
+    var timeLeft = endTime - now;
+
+    var days = Math.floor(timeLeft / 86400);
+    var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
+    var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600 )) / 60);
+    var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+
+    if (hours < "10") { hours = "0" + hours; }
+    if (minutes < "10") { minutes = "0" + minutes; }
+    if (seconds < "10") { seconds = "0" + seconds; }
+
+    $("#days").html(days);
+    $("#hours").html(hours);
+    $("#minutes").html(minutes);
+    $("#seconds").html(seconds);
+
+  }
+
+  setInterval(function() { makeTimer(); }, 1000);
+
+
+});
